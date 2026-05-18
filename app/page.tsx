3dfd916aeaa1c300d8d35428b5c4c8b1b6c50be9
nasti <anastasiasaienko634@gmail.com>
2026-05-18 21:39:42 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import css from "./page.module.css";
+import background from "../public/backgroundImg.png";
 
 // Home Page
 const Home = () => {
   return (
     <main className={css.main}>
-      <div className={`${css.heroContainer} ${css.fadeUp}`}></div>
+      <div className={`${css.heroContainer} ${css.fadeUp}`}>
+        <img src={background} alt="" />
+      </div>
     </main>
   );
 };
