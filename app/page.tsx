cd617dfd64f19dcddabb59e2dc9e03e55f333e2b
nasti <anastasiasaienko634@gmail.com>
2026-05-19 21:27:31 +0200

    update HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
+import Image from "next/image";
 import css from "./page.module.css";
 import background from "../public/backgroundImg.png";
+import Header from "@/components/Header/Header";
 
 // Home Page
 const Home = () => {
   return (
     <main className={css.main}>
-      <div className={`${css.heroContainer} ${css.fadeUp}`}>
-        <img src={background} alt="" />
+      <div>
+        <div className={css.heroContainer}>
+          <Header />
+          <h1 className={css.titleHero}>
+            Take good <span className={css.markedText}>care</span> of your small
+            pets
+          </h1>
+          <p className={css.descriptionHero}>
+            Choosing a pet for your home is a choice that is meant to enrich
+            your life with immeasurable joy and tenderness.
+          </p>
+        </div>
+        <Image
+          className={css.backgroundImg}
+          src={background}
+          alt="Background"
+          priority
+        />
       </div>
     </main>
   );
